f6b96d76c5d2310f067c13501251ea29c44da00f
PoloBustillo <leopoldobeuiluz1@hotmail.com>
2023-09-19 20:55:23 -0600

    tarea5

---
+++ tarea5/Catedratico.java
@@ -0,0 +1,43 @@
+package tarea5;
+
+public class Catedratico extends Persona {
+    private String facultad;
+    private float salario;
+
+    public Catedratico(String nombre, String residencia, String facultad, float salario) {
+        super(nombre, residencia);
+        this.facultad = facultad;
+        this.salario = salario;
+    }
+
+
+    public Catedratico(String nombre, String residencia) {
+        super(nombre, residencia);
+    }
+
+    public float pagoCuotaSS(float salario) {
+        return (float) (salario * .15);
+    }
+
+    public void imprimeCatedratico() {
+        imprimePersona();
+        System.out.println("facultad = " + facultad);
+        System.out.println("salario = " + salario);
+    }
+
+    public String getFacultad() {
+        return facultad;
+    }
+
+    public void setFacultad(String facultad) {
+        this.facultad = facultad;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+}
